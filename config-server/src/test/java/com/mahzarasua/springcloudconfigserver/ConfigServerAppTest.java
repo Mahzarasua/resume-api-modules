798acb0e0d4b8160f50ffc6f2ed2ec3a555283f2
Mike Zars <mahzarasua@hotmail.com>
2022-10-27 13:36:17 -0500

    Initial tests for ConfigServer Module

---
+++ config-server/src/test/java/com/mahzarasua/springcloudconfigserver/ConfigServerAppTest.java
@@ -0,0 +1,13 @@
+package com.mahzarasua.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ConfigServerAppTest.class)
+class ConfigServerAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
