91b133e81ef85ed29938024b479b0abf3dfc546c
Mike Zars <mahzarasua@hotmail.com>
2022-10-27 19:05:36 -0500

    Update to tests to make them standard across the modules

---
+++ resume/src/test/java/com/mahzarasua/resumeapi/ResumeAppTest.java
@@ -0,0 +1,13 @@
+package com.mahzarasua.resumeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ResumeApp.class)
+class ResumeAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
